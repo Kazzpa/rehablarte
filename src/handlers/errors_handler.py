153dbe00f11d3c689f3fea007068366a0d49f296
german <geralejo95@gmail.com>
2025-12-13 00:05:32 +0100

    Aiogram error handler added

---
+++ src/handlers/errors_handler.py
@@ -0,0 +1,25 @@
+from loguru import logger
+from aiogram import Router
+from aiogram.types import Update, ErrorEvent
+
+# Define a router to capture all exceptions
+errors_router = Router()
+
+"""
+This handler will manage all global errors, for specific behaviours or custom exception add try/catch in command/event
+"""
+@errors_router.errors()
+async def errors_handler(event: ErrorEvent):
+    update: Update = event.update
+    ex: Exception = event.exception
+    cause = getattr(ex, "__cause__", None)
+    if cause:
+        logger.error(f"Error ocurred: {cause}")
+    else:
+        logger.error(f"Internal Error - unkown cause {ex}")
+    try:
+        if update.message:
+            await update.message.answer("❌ Ocurrió un error interno.")
+    except Exception as send_error:
+        logger.warning(f"Failed to send error message: {send_error}")
+
