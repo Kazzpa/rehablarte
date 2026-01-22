3889b5cf2819ca2066696daf748d0cf727727270
german <geralejo95@gmail.com>
2026-01-22 17:20:39 +0100

    initialization done & event for new users

--- src/handlers/event_handler.py
+++ src/handlers/event_handler.py
@@ -1,76 +1,65 @@
-from aiogram import Router
+from aiogram import Router, Bot
 from aiogram.types import ChatMemberUpdated
 from aiogram.fsm.context import FSMContext
 from loguru import logger
-from models.chat_entity import ReChat, ReChatSession
-from utils.redis_repository import RedisRepository, ReChatRepository
+from datetime import datetime
+from models.chat_entity import ReChatGroup, ReChatMember
+from utils.redis_repository import RedisRepository
 from modules.redis_db import redis_client
-from utils.session import get_session, update_session
+from utils.session import get_session, get_member_from_chat, update_session, check_session_persistance
 
 event_router = Router()
 redis_repo = RedisRepository(redis_client)
 
 # Manage bot/users joining a chat group or leaving a chat group
 @event_router.my_chat_member()
-async def chat_group_member_event(event: ChatMemberUpdated, state: FSMContext):
+async def chat_group_member_event(event: ChatMemberUpdated, state: FSMContext, bot: Bot):
     logger.info("User leaving/joining detected")
 
-    chat_id = event.chat.id
+    chat = event.chat
     user = event.new_chat_member.user
 
-    if event.chat.type == "private":
+    if(user.is_bot and user.id == bot.id):
+        logger.info("User affected is self - Rehablarte Bot - Skipping")
+        return
+
+    if (chat.type == "private"):
         logger.warning("This functionality is not available for private chats")
-        # TODO: Maybe save new private chat here
         return
 
-    # load repository
-    chat_repo = ReChatRepository(redis_repo)
-
-    # Get current session
     session = await get_session(state)
-    re_chat = session.chat
-
-    # If current chat is not found in db then create new and save it
-    if re_chat is None:
-        logger.info("Session does not have a chat saved, searching db...")
-        re_chat = await chat_repo.get(chat_id=chat_id)
-        if not re_chat:
-            logger.info("Chat not found in DB, creating new chat...")
-            re_chat = ReChat(id=chat_id, active=False, chat_members=[], type="group")
-            await chat_repo.save(re_chat)
-
-        session.chat = ReChatSession(re_chat)
-        session.initialized = True
-
-    # If we have a session check initialize status
-    if not session.initialized:
-        re_chat = session.chat
-
-        session.initialized = True
-        session.chat = re_chat
-        await state.update_data(session)
-
-    # Check type of user
-    if user.is_bot:
-        # TODO: Initialize session for bot
-        logger.info("User is bot skipping...")
-        return
-
-    
+    session_chat: ReChatGroup = session.chat
+    new_status = event.new_chat_member.status
+    old_status = event.old_chat_member.status
 
     # Check if user is leaving/joining
-        # to check new joined
-        # check new_chat_member status is member & old is left or other maybe
-        # to check someone left or kicked
-        # check new is left old is member
+    # TODO: Check if is necessary to check admin status
+    # new user
+    if(new_status == "member"):
+        logger.info("New user detected")
+        new_member = await get_member_from_chat(chat, user.id)
+        # Si no existia lo creamos
+        if new_member is None:
+            new_member = ReChatMember(id=user.id, username=user.username, status=new_status, joinedAt=datetime.now().date(), isBot=user.is_bot)
+        # Actualizamos estado y guardamos
+        else:
+            new_member.status = new_status
+        
+    # user leaving
+    elif(event.new_chat_member.status in ["left", "kicked"]):
+        logger.info("User leaving group detected")
+        # Search for user in db and delete it
+        old_member = await get_member_from_chat(chat, user.id)
+        # if user is not in db skip
+        if old_member is None:
+            logger.warning("The user was not found when trying to delete")
+            return
+        
+        session_chat.chat_members.remove(old_member)
 
-    # Update DB creating/deleting corresponding info
-
-
-    id = event.chat.id
-    new_chat = ReChat(id)
-
-    chat_repo = ReChatRepository(redis_repo)
-    logger.info(new_chat)
-    await chat_repo.save(new_chat)
-
+    # Update session & set to dirty
+    await update_session(state=state, session=session)
+    session.is_dirty = True
+    # Save changes into persistance
+    await check_session_persistance(state=state)
+    
--- src/utils/session.py
+++ src/utils/session.py
@@ -1,10 +1,37 @@
 from loguru import logger
 from aiogram.fsm.context import FSMContext
 from aiogram.types.chat import Chat
-from models.chat_entity import ReChatSession, ReChat, RePrivateChat, ReChatGroup
+from models.chat_entity import ReChatSession, ReChat, RePrivateChat, ReChatGroup, ReChatMember
 from datetime import datetime
 from utils.redis_repository import ReSessionRepository, RedisRepository
 from modules.redis_db import redis_client
+
+# This function should check whether the chat is in session and then whether is saved in persistance
+async def check_session_persistance(state: FSMContext):
+    logger.info("Checking chat persistance")
+    session = await get_session(state)
+    if session is None:
+        logger.error("Error - Session is not initialized")
+        # TODO: Maybe create custom exception
+        raise Exception("Session not valid")
+    
+    if not session.is_dirty:
+        logger.info("Session is not diry - skipping persistance")
+        return
+
+    logger.info(f"Persisting session {session.chat.id} to DB")
+    redis_repo = RedisRepository(redis_client)
+    session_repo = ReSessionRepository(redis_repo)
+    await session_repo.save(session=session)
+    session.is_dirty = False
+    await update_session(session=session)
+
+# This function should return a member from the session chat, it assumes you already got the session and chat
+async def get_member_from_chat(chat: ReChatGroup, idOrUsername: str) -> ReChatMember | None:
+    logger.info("Searching member in session")
+    member_list = chat.chat_members
+    return next(filter(lambda member: member.id == idOrUsername | member.username == idOrUsername, member_list))
+
 
 async def get_session(state: FSMContext) -> ReChatSession:
     logger.info("Getting session")
@@ -14,11 +41,11 @@ async def get_session(state: FSMContext) -> ReChatSession:
         logger.info("No session detected, creating session...")
         return ReChatSession()
      
-    return ReChatSession.model_validate(rawSession)
+    return ReChatSession.model_validate_json(rawSession)
 
 async def update_session(state: FSMContext, session: ReChatSession):
     logger.info("Updating session...")
-    await state.update_data(chat_session=session.model_dump())
+    await state.update_data(chat_session=session.model_dump_json())
 
 async def initialize_session(state: FSMContext, chat: Chat):
     logger.info("Initializing session")
@@ -33,20 +60,15 @@ async def initialize_session(state: FSMContext, chat: Chat):
 
     if chat_type is None:
         logger.info("Initialize empty chat")
-        chat = ReChat(id=chat_id, firstInteraction=datetime.now())
+        new_chat = ReChat(id=chat_id, firstInteraction=datetime.now())
     elif chat_type == "private":
         logger.info("Initializing session as private")
         if username is None:
             logger.error("Error username in a private chat cannot be None")
             raise Exception("Error username is None")
-        chat = RePrivateChat(id=chat_id, firstInteraction=datetime.now(), username=username)
+        new_chat = RePrivateChat(id=chat_id, firstInteraction=datetime.now().date(), username=username)
     elif chat_type in ["group", "supergroup"]:
         logger.info("Initializing session as group chat")
-        chat = ReChatGroup(id=chat_id, firstInteraction=datetime.now(), chat_members=[], type=chat_type)
-    new_session = ReChatSession(initialized=True, chat=chat)
+        new_chat = ReChatGroup(id=chat_id, firstInteraction=datetime.now().date(), chat_members=[], type=chat_type)
+    new_session = ReChatSession(initialized=True, chat=new_chat)
     await update_session(state=state, session=new_session)
-
-    # TODO: REMOVE THIS; ONLY TESTING
-    redis_repo = RedisRepository(redis_client)
-    session_repo = ReSessionRepository(redis_repo)
-    session_repo.save(new_session)
